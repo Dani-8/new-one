9b4ec0fb82f73e35f0f02297ed47886e70612917
Dani-8 <daniyalw6190@gmail.com>
2026-07-08 22:57:33 +0500

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,18 @@
+import AdminPanel from './features/admin/AdminPanel'
 
+function App(){
+    return(
+        <AdminPanel 
+            foods={[]}
+            orders={[]}
+            onUpdateOrderStatus={() => {}}
+            onAddFood={() => {}}
+            onUpdateFood={() => {}}
+            onDeleteFood={() => {}}
+            isDarkMode={false}
+        />
+    )
+}
 
 
 
